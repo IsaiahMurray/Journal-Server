13c430c3f3dd065e6545d24eb78074838f5ba21d
Isaiah Murray <murray.isaiah13@gmail.com>
2021-06-07 16:45:12 -0400

    changed env variables

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const {Sequelize} = require("sequelize");
 
-const db = new Sequelize(process.env.DB_CONNECTION_STRING);
+const db = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = db;
