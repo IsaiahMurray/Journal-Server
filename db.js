571d1135ae48f50ad428be11bf6de767e66f730e
Isaiah Murray <murray.isaiah13@gmail.com>
2021-06-08 22:58:38 -0400

    Deploying

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const {Sequelize} = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL);
+const db = new Sequelize(process.env.DATABASE_URL, {dialect: 'postgres'});
 
 module.exports = db;
