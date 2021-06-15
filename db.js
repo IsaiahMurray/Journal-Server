52bcc2547f9e54c04e41b85d3f8ef23bca32f0ef
Isaiah Murray <murray.isaiah13@gmail.com>
2021-06-15 09:12:06 -0400

    changes ssl in db

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
-require('dotenv').config();
-const {Sequelize} = require("sequelize");
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    ssl: process.env.ENVIRONMENT === 'production'
+  dialect: "postgres",
+  ssl: {
+    dialectOptions: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = db;
