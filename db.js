d4b73008606f1ef2c3dc66892dde62026b5ec9dd
Isaiah Murray <murray.isaiah13@gmail.com>
2021-06-24 14:20:03 -0400

    Added db connection comment

--- db.js
+++ db.js
@@ -11,4 +11,9 @@ const db = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
+// const db = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: 'postgres',
+//   ssl: process.env.ENVIRONMENT === 'production'
+// })
+
 module.exports = db;
